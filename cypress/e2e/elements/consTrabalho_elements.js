7013afa4486f06358b71510bbc06a652ee843767
Guilherme Porto <portoengenheiro@gmail.com>
2025-01-22 15:09:33 -0300

    Inclusão dos identificadores na classe elements

--- cypress/e2e/elements/consTrabalho_elements.js
+++ cypress/e2e/elements/consTrabalho_elements.js
@@ -1,3 +1,13 @@
 class ConsultarTrabalhoElements {
+    pesquisarTrabalho = () => {return 'input[placeholder="Pesquisar"]'}
+    ordenarInscricao = () => {return 'nz-table-sorters[ng-reflect-sort-directions="ascend,descend,"] span'.eq(0)}
+    ordenarTitulo = () => {return 'span.ant-table-column-title'.eq(1)}
+    ordenarTematica = () => {return 'span.ant-table-column-title'.eq(2)}
+    ordenarCategoria = () => {return 'nz-table-sorters[ng-reflect-sort-directions="ascend,descend,"]'.eq(3)}
+    ordenarStatus = () => {return 'span.ant-table-column-title'.eq(4)}
+    btnVisualizarTrabalho = () => {return 'button[ng-reflect-title="Visualizar trabalho"]'.eq(0)}
+    btnProximaPagina = () => {return 'li[ng-reflect-type="next"] button' }
+    btnPaginaAnterior = () => {return 'li[title="Página anterior"] button' }
+    defineQuantidadeDeItemApresentados = () => {return 'nz-select-item[ng-reflect-deletable="false"]'}
 
 } export default ConsultarTrabalhoElements
